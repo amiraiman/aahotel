d9cd4cfc21c0b4abea1bcb87f8a20df5fff3ecde
Amir Aiman <amiraiman.jj@gmail.com>
2022-02-22 10:59:32 +0800

    Serve static files underneath public route and fix all html links

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+const path = require("path");
 const express = require("express");
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
