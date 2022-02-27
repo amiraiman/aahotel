880a2373d26bf3aa4af5bd42481311f1fd5284ac
Amir Aiman <amiraiman.jj@gmail.com>
2022-02-27 12:04:43 +0800

    Toggle nav-open class when nav button is clicked

--- public/js/misc.js
+++ public/js/misc.js
@@ -1,2 +1,8 @@
 const year = new Date().getFullYear();
 const footerYear = (document.querySelector("#footer-year").textContent = year);
+
+// Toggle nav-open class on nav button onclikc
+const navBtn = document.querySelector(".header-icon");
+navBtn.addEventListener("click", (e) => {
+  e.target.closest(".header").classList.toggle("nav-open");
+});
